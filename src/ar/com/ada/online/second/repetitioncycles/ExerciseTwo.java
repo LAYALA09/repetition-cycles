284bde802c26515788c36d1f15cd1039b892cc75
Liliana <ayalilian2018@gmail.com>
2020-08-12 00:10:44 -0300

    Finish exercise two

--- src/ar/com/ada/online/second/repetitioncycles/ExerciseTwo.java
+++ src/ar/com/ada/online/second/repetitioncycles/ExerciseTwo.java
@@ -1,4 +1,5 @@
 package ar.com.ada.online.second.repetitioncycles;
+//Realice y represente  el producto de dos matrices de orden M x N y P x Q.
 
 import java.util.Scanner;
 
@@ -7,7 +8,94 @@ public class ExerciseTwo {
 
 
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Suma de vectores");
+        //int arrayA[][];
+
+        //int arrayB[][] ;
+        //int arrayC[][];
+        int rowB;
+        int columnB;
+
+        int rowA;
+        int columnA;
 
 
+        //Ingreso datos A
+        System.out.println(" Ingrese el tamaño de la fila de la matriz A");
+        rowA = keyboard.nextInt();
+        System.out.println(" Ingrese el tamaño de la Columna de la matriz A");
+        columnA = keyboard.nextInt();
+
+        //Ingreso datos B
+        System.out.println(" Ingrese el tamaño de la fila de la matriz B");
+        rowB = keyboard.nextInt();
+        System.out.println(" Ingrese el tamaño de la Columna de la matriz B");
+        columnB = keyboard.nextInt();
+
+
+        //Cargar la matriz A
+        if (rowA == columnB) {
+            int arrayA[][] = new int[rowA][columnA];
+            int arrayB[][] = new int[rowB][columnB];
+            int arrayC[][] = new int[rowA][columnB];
+            System.out.println("Digite la matriz A");
+            for (int i = 0; i < rowA; i++) {
+                for (int j = 0; j < columnA; j++) {
+                    System.out.println("arrayA[ " + i + " ][ " + j + " ] : ");
+                    arrayA[i][j] = keyboard.nextInt();
+
+                }
+                System.out.println(" ");
+            }
+            //Cargar la matriz B
+            System.out.println("Digite la matriz B");
+            for (int i = 0; i < rowB; i++) {
+                for (int j = 0; j < columnB; j++) {
+                    System.out.print("arrayB[ " + i + " ][ " + j + " ] : ");
+                    arrayB[i][j] = keyboard.nextInt();
+                }
+                System.out.println(" ");
+            }
+            //muestro la matriz A
+            System.out.println("Matriz A :");
+            for (int i = 0; i < rowA; i++) {
+                for (int j = 0; j < columnA; j++) {
+                    System.out.print(arrayA[i][j] + " ");
+
+
+                }
+                System.out.println(" ");
+            }
+            //muestro la matriz B
+            System.out.println("Matriz B :");
+            for (int i = 0; i < rowB; i++) {
+                for (int j = 0; j < columnB; j++) {
+                    System.out.print(arrayB[i][j] + " ");
+
+
+                }
+                System.out.println(" ");
+
+
+            }
+            //Matriz C
+            System.out.println("Matriz C que se obtiene de A*B ");
+            for (int i = 0; i < rowA; i++) {
+                for (int j = 0; j < columnB; j++) {
+                    for (int n = 0; n < columnA; n++) {
+                        arrayC[i][j] += arrayA[i][n] * arrayB[n][j];
+
+                    }
+                    System.out.print(arrayC[i][j] + " ");
+                }
+                System.out.println(" ");
+            }
+
+        } else {
+            System.out.println("La operación no se puede realizar");
+        }
+
+    }
 }
+
+
+
