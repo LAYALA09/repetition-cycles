5779f85315e794bba2acf5e6dbd787ec247b9c01
Liliana <ayalilian2018@gmail.com>
2020-08-16 15:32:49 -0300

    Finish exercise fifteen

--- src/ar/com/ada/online/second/repetitioncycles/ExerciseFifteen.java
+++ src/ar/com/ada/online/second/repetitioncycles/ExerciseFifteen.java
@@ -1,6 +1,7 @@
 package ar.com.ada.online.second.repetitioncycles;
 //Calcule el producto de dos vectores. Uno de ellos es de una fila
-con diez elementos y el otro con una columna de diez elementos
+
+//con diez elementos y el otro con una columna de diez elementos
 
 import java.util.Scanner;
 
@@ -10,7 +11,7 @@ public class ExerciseFifteen {
         int arrayA[] = new int[10];
         int arrayB[] = new int[10];
         int arrayC[] = new int[10];
-//carga de vector A
+      //carga de vector A
         System.out.println("Vector A");
         for (int i = 0; i < 10; i++) {
             System.out.println("Digite valor ");
@@ -22,10 +23,12 @@ public class ExerciseFifteen {
             System.out.print("Digite valor ");
             arrayB[i] = keyboard.nextInt();
         }
+        //Producto de vectores
         System.out.println("Vector C");
         for (int i = 0; i < 10; i++) {
             arrayC[i] = arrayA[i] * arrayB[i];
         }
+        //Muestro vector resultante
         for (int i = 0; i < 10; i++) {
             System.out.println(arrayC[i] + " ");
         }
